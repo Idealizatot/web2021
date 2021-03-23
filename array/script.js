7dddcbf1fc4ff228377a29f5b45a9360e80ba5b9
AlexM <AlexMtkhn@yandex.ru>
2021-03-23 21:05:26 +0300

    вот такой вариантик

---
+++ array/script.js
@@ -0,0 +1,39 @@
+buttonField.onclick = function () {
+    let strArray = inputField.value.split("");
+    console.log("Even numbers: " + getEvenNumbers(strArray));
+    console.log("Squares numbers: " + getSquaresNumbers(strArray));
+    console.log("Three Multiples Sum: " + getThreeMultiplesSum(strArray));
+}
+
+function getEvenNumbers(strArray){
+    let evenNumberStr = "";
+    for(let str of strArray){
+        let num = Number(str);
+        if (num % 2 === 0){
+            evenNumberStr += str;
+        }
+    }
+    return evenNumberStr;
+}
+
+function getSquaresNumbers(strArray){
+    let evenNumberStr = "";
+    for(let str of strArray){
+        evenNumberStr += String(Math.pow(Number(str), 2)) + " ";
+    }
+    return evenNumberStr;
+}
+
+function getThreeMultiplesSum(strArray){
+    let evenNumberStr = 0;
+    for(let str of strArray){
+        let num = Number(str);
+        if (num % 3 === 0){
+            evenNumberStr += num;
+        }
+    }
+    return evenNumberStr;
+}
+
+
+//document.addEventListener("click", eventHandler);
