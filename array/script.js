b3bc5d0174629d92a92f53f2c69735c889dfccc7
AlexM <AlexMtkhn@yandex.ru>
2021-03-24 22:38:27 +0300

    навешиваем событие на кнупку так, чтоб не перетирать другие

--- array/script.js
+++ array/script.js
@@ -1,38 +1,42 @@
-
 document.addEventListener("DOMContentLoaded", ()=> {
 
-buttonField.onclick = function () {
+    buttonField.addEventListener('click', writeToConsole);
+
+    function writeToConsole() {
     console.log("Even numbers: " + getEvenNumbers(inputField.value));
     console.log("Squares numbers: " + getSquaresNumbers(inputField.value));
     console.log("Three Multiples Sum: " + getThreeMultiplesSum(inputField.value));
-}
-
-function getEvenNumbers(inputFieldValue){
-    let evenNumbers = "";
-    for(let char of inputFieldValue){
-        if (char.valueOf() % 2 === 0){
-            evenNumbers += char;
-        }
     }
+
+    function getEvenNumbers(inputFieldValue){
+    
+        let evenNumbers = "";
+   
+        for(let char of inputFieldValue){
+            if (char.valueOf() % 2 === 0){
+                evenNumbers += char;
+            }
+        }
     return evenNumbers;
-}
-
-function getSquaresNumbers(inputFieldValue){
-    let numbersSquares = "";
-    for(let char of inputFieldValue){
-        numbersSquares += String(Math.pow(char.valueOf(), 2)) + " ";
     }
-    return numbersSquares;
-}
 
-function getThreeMultiplesSum(inputFieldValue){
-    let threeMultiplesSum = 0;
-    for(let char of inputFieldValue){
-        let num = Number(char);
-        if (num % 3 === 0){
-            threeMultiplesSum += num;
+    function getSquaresNumbers(inputFieldValue){
+        let numbersSquares = "";
+        for(let char of inputFieldValue){
+            numbersSquares += String(Math.pow(char.valueOf(), 2)) + " ";
         }
+        return numbersSquares;
     }
-    return threeMultiplesSum;
-}
+
+
+    function getThreeMultiplesSum(inputFieldValue){
+        let threeMultiplesSum = 0;
+        for(let char of inputFieldValue){
+            let num = Number(char);
+            if (num % 3 === 0){
+                threeMultiplesSum += num;
+            }
+        }
+        return threeMultiplesSum;
+    }
 });
