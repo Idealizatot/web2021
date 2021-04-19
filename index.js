011b64636f34cb50eeeaf389962cc7a606452666
AlexM <AlexMtkhn@yandex.ru>
2021-04-20 00:32:11 +0300

    начало

---
+++ index.js
@@ -0,0 +1,9 @@
+buttonField1.addEventListener('click', writeToConsole);
+
+let buttonTeg = document.getElementById("buttonField");
+buttonTeg.addEventListener('click', display);
+
+
+let interface = new Object(){
+    oneButton = 
+}
